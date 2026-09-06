7f2e5705c63796b6ab0acd20d4d4829e1b4eb5c6
sagar1641999 <sagar@onelap.in>
2026-09-06 12:17:01 +0530

    new_changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'screens/home_screen.dart'; // Aapki UI screen yahan import ho rahi hai
+import 'screens/home_screen.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
